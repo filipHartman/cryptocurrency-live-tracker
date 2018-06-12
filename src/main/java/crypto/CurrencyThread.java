ebbfecfab69886bcbadedea81119fcdcd44120af
DrMayx <mj6367@gmail.com>
2018-06-12 14:32:03 +0200

    add list of data for currency

--- src/main/java/crypto/CurrencyThread.java
+++ src/main/java/crypto/CurrencyThread.java
@@ -1,12 +1,16 @@
 package crypto;
 
+import java.util.HashMap;
+
 public class CurrencyThread extends Thread {
 
     public  String name;
+    public HashMap<String, String> data;
 
     public CurrencyThread(String cryptoName){
         super();
         this.name = cryptoName;
+        this.data = new HashMap<>();
     }
 
     public void run(){
