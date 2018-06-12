30fac93262a7f5ebe3b80837da8bda48c3962c3f
filipHartman <filip.hartman23@gmail.com>
2018-06-12 11:04:16 +0200

    Add service to create websites url with currency data

---
+++ src/main/java/WebData/URLCreatorService.java
@@ -0,0 +1,9 @@
+package WebData;
+
+public class URLCreatorService {
+    public static final String SOURCE_URL = "http://min-api.cryptocompare.com/data/";
+    public static final String PRICES_COMMAND = "pricemulti";
+    public static final String SYMBOLS_OF_INTEREST = "fsyms";
+    public static final String SYMBOLS_TO_CONVERT = "tsyms";
+
+}
