cf49927c833260b30ad672f93cc7bc876f3fa576
filipHartman <filip.hartman23@gmail.com>
2018-06-12 11:39:38 +0200

    Displaying url - test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,8 @@
+import WebData.URLCreatorService;
+
 public class App {
     public static void main(String[] args) {
-
+        URLCreatorService dataUrl = new URLCreatorService();
+        System.out.println(dataUrl.createURL(dataUrl.DEFAULT_CRYPTOCURRENCY_SYMBOLS, new String[]{"USD", "EUR"}));
     }
 }
