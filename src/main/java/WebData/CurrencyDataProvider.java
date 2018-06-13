67f3239dad5b51b2ebeccd436a73510a05ee29bc
filipHartman <filip.hartman23@gmail.com>
2018-06-13 13:21:06 +0200

    Add getting list with currenct threads from data from website

--- src/main/java/WebData/CurrencyDataProvider.java
+++ src/main/java/WebData/CurrencyDataProvider.java
@@ -1,34 +1,55 @@
 package WebData;
 
+import crypto.CurrencyThread;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Arrays;
+import java.util.*;
 
 public class CurrencyDataProvider {
     private static URLCreatorService urlCreator = new URLCreatorService();
 
-    public static void main(String[] args) throws Exception {
-        String url = urlCreator.createURL(urlCreator.DEFAULT_CRYPTOCURRENCY_SYMBOLS, new String[]{"USD", "EUR", "PLN"});
+    private static final String[] DEFAULT_CRYPTOCURRENCY_SYMBOLS = new String[] {
+            "BTC", "ETH", "XRP", "BCH", "ADA", "XLM", "NEO", "LTC", "EOS", "XEM"
+    };
 
-        URL oracle = new URL(url);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(oracle.openStream()));
-        String result = "";
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            result = inputLine;
-        in.close();
-        System.out.println(result);
-        String newResult = result.substring(1,result.length()-1);
-        System.out.println(newResult);
-        System.out.println(Arrays.toString(newResult.split("},")));
-        for(String cryptoCurrency: newResult.split("},")) {
-            String[] splitedCurrencies = cryptoCurrency.split(":\\{");
-            String cryptoCurrencyName = splitedCurrencies[0];
-            String realCurencies = splitedCurrencies[1];
-            System.out.println(cryptoCurrencyName);
-            System.out.println(realCurencies);
+    public static String[] getDefaultCryptocurrencySymbols() {
+        return DEFAULT_CRYPTOCURRENCY_SYMBOLS;
+    }
+
+    public List<CurrencyThread> getCurrencyData(String[] cryptoCurrencies, String[] realCurrencies) {
+        List<CurrencyThread> currencies = new ArrayList<>();
+        String urlAddress = urlCreator.createURL(cryptoCurrencies, realCurrencies);
+        URL dataUrl;
+        try {
+            dataUrl = new URL(urlAddress);
+            BufferedReader in = new BufferedReader(new InputStreamReader(dataUrl.openStream()));
+            String inputLine;
+            String webData = "";
+            if ((inputLine = in.readLine()) != null) {
+                webData = inputLine;
+                System.out.println(webData);
+            }
+            String[] allCryptoCurrenciesData = webData.substring(1, webData.length() - 1).split("},");
+            for (String cryptoCurrency : allCryptoCurrenciesData) {
+
+                String[] splitedCurrencies = cryptoCurrency.split(":\\{");
+                String cryptoCurrencyName = splitedCurrencies[0];
+                String[] realCurrenciesData = splitedCurrencies[1].split(",");
+                HashMap<String, String> realCurrenciesMap = new HashMap<>();
+                for (String realCurrency : realCurrenciesData) {
+                    String[] splitedRealCurrency = realCurrency.split(":");
+                    realCurrenciesMap.put(splitedRealCurrency[0], splitedRealCurrency[1].replace("}",""));
+                }
+                currencies.add(new CurrencyThread(cryptoCurrencyName, realCurrenciesMap));
+
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return currencies;
     }
 }
