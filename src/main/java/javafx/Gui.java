a7f79c68c019193fc33b6c58d4971bc0750d088a
Adrian Bala <aadrianbala@gmail.com>
2018-06-13 13:29:22 +0200

    Add layout to GUI

--- src/main/java/javafx/Gui.java
+++ src/main/java/javafx/Gui.java
@@ -4,9 +4,9 @@ import crypto.CurrencyContainer;
 import crypto.CurrencyThread;
 
 import java.awt.*;
-import java.awt.event.*;
 import java.util.List;
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
 
 public class Gui extends JFrame {
 
@@ -27,27 +27,32 @@ public class Gui extends JFrame {
 
         threads.setVisibleRowCount(10);
         threads.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
+        JTextField logo = new JTextField("SUPER DUPER CRYPTO LIVE TRACKER");
+        logo.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 30));
+        logo.setEditable(false); // powiekszyc logo na cala szerokosc
+        add(logo);
+
+        String[] currencies = {"BTC", "ETH"};
+        JComboBox dropdownMenu = new JComboBox(currencies);
+        dropdownMenu.setAlignmentX(0);
+        add(dropdownMenu);
+
+        JInternalFrame checkboxPanel = new JInternalFrame();
+        checkboxPanel.setVisible(true);
+        checkboxPanel.setLayout(new GridLayout(2,2));
+        checkboxPanel.add(new JCheckBox("EUR"));
+        checkboxPanel.add(new JCheckBox("USD"));
+        checkboxPanel.add(new JCheckBox("PLN"));
+
+        checkboxPanel.setBorder(null);
+        BasicInternalFrameUI bi = (BasicInternalFrameUI)checkboxPanel.getUI();
+        bi.setNorthPane(null);
+        add(checkboxPanel);
+
+        JButton followButton = new JButton("Follow");
+        add(followButton);
+
         add(new JScrollPane(threads));
-
-        item1 = new JTextField(10);
-        item1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String input = e.getActionCommand();
-                toTest.add(new CurrencyThread(input));
-            }
-        });
-        add(item1);
-
-        button = new JButton("Add crypto");
-        button.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent event) {
-                        System.out.println("dziala");
-                    }
-                }
-        );
-        add(button);
-
     }
 }
