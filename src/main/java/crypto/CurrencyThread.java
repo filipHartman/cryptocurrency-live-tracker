69dd287fa1fe2f571a01c5dab22180496e4a036a
Adrian Bala <aadrianbala@gmail.com>
2018-06-14 11:33:40 +0200

    Change thread sleep time ic currency thread

--- src/main/java/crypto/CurrencyThread.java
+++ src/main/java/crypto/CurrencyThread.java
@@ -26,7 +26,7 @@ public class CurrencyThread extends Thread {
         while(true){
             this.data = dataProvider.getCurrencyData(name, realCurrencies);
             try {
-                Thread.sleep(1000);
+                Thread.sleep(10000);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
